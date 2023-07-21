d74f9ea17ab88b006e72fdecaad94c12fa51ef53
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-07-21 10:50:39 +0800

    chore: 运行成功

--- backend/hadoop/src/test/java/zlhywlf/hd/HadoopTest.java
+++ backend/hadoop/src/test/java/zlhywlf/hd/HadoopTest.java
@@ -1,8 +1,11 @@
 package zlhywlf.hd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -10,15 +13,28 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+@Slf4j
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class HadoopTest {
 
-    @Test
-    void base() throws URISyntaxException, IOException, InterruptedException {
-        Configuration configuration = new Configuration();
-        FileSystem fs = FileSystem.get(new URI("hdfs://http://192.168.50.54:9000"),configuration,"root");
-        fs.mkdirs(new Path("/test"));
+    Configuration configuration;
+    FileSystem fs;
+
+    @BeforeAll
+    void init() throws URISyntaxException, IOException, InterruptedException {
+        configuration = new Configuration();
+        fs = FileSystem.get(new URI("hdfs://192.168.50.54:9000"), configuration, "root");
+    }
+
+    @AfterAll
+    void close() throws IOException {
         fs.close();
+    }
+
+    @Test
+    void base() throws IOException {
+        boolean isOk = fs.mkdirs(new Path("/test"));
+        log.info("创建目录: {}", isOk);
     }
 
 }
