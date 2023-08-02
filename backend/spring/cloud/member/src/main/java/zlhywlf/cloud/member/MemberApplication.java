e7ac8e27a75c07d1be8881d2d71829f5c2c57d4c
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-08-02 15:22:29 +0800

    chore: 添加公共模块

--- backend/spring/cloud/member/src/main/java/zlhywlf/cloud/member/MemberApplication.java
+++ backend/spring/cloud/member/src/main/java/zlhywlf/cloud/member/MemberApplication.java
@@ -3,17 +3,18 @@ package zlhywlf.cloud.member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan("zlhywlf.cloud")
 public class MemberApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(MemberApplication.class);
         ConfigurableEnvironment env = app.run(args).getEnvironment();
-        String port = env.getProperty("server.port");
-        log.info("http://localhost:{}/member", port == null ? 8080 : port);
+        log.info("http://localhost:{}{}", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 
 }
