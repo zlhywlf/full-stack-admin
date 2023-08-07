d1968602763cd6356ad96483a9ed72be887f7484
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-08-07 11:40:39 +0800

    chore: 钉钉机器人

---
+++ backend/py/robot.py
@@ -0,0 +1,28 @@
+import requests
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
+import json
+
+
+class Config:
+    """
+    https://open.dingtalk.com/document/orgapp/custom-robot-access
+    """
+
+    def __init__(self, url: str, secret: str, access_token: str):
+        timestamp = str(round(time.time() * 1000))
+        secret_enc = secret.encode('utf-8')
+        string_to_sign = '{}\n{}'.format(timestamp, secret)
+        string_to_sign_enc = string_to_sign.encode('utf-8')
+        hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+        sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+        self.url = url + '?access_token=%s&timestamp=%s&sign=%s' % (access_token, timestamp, sign)
+
+
+def notice(config: Config, data: dict):
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(config.url, headers=headers, data=json.dumps(data))
+    print(json.loads(response.text))
