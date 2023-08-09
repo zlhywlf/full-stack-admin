b6265075f55b6f0c4ec54a3cbeba108e2b71cfd3
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-08-09 14:52:40 +0800

    chore: json 工具类

--- backend/cpp/json/test/jsonTest.cc
+++ backend/cpp/json/test/jsonTest.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "mapper/RapidjsonAdapter.H"
+#include "json/util/JsonUtil.H"
 
 using std::cout;
 using std::endl;
@@ -8,7 +8,8 @@ using std::endl;
 int main(int argc, char const *argv[]) {
   const char *json =
       R"({"hello":"world","t":true,"n":null,"i":123,"pi":3.1415926,"a":[0,1,2,3]})";
-  zlhywlf::json::RapidjsonAdapter adapter(json);
-  assert(adapter.value().HasMember("hello"));
+  int i;
+  zlhywlf::json::JsonUtil::readValue(i, json);
+  assert(i == 123);
   return 0;
 }
