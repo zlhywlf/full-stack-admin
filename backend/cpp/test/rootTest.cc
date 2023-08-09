9bf356d076b4077192cbec99253c49d90528d8a3
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-08-09 18:46:10 +0800

    chore: 日志支持

--- backend/cpp/test/rootTest.cc
+++ backend/cpp/test/rootTest.cc
@@ -1,9 +1,18 @@
 #include <iostream>
 
+#include "logger/DefaultLoggerFactory.H"
+
 using std::cout;
 using std::endl;
 
 int main(int argc, char const *argv[]) {
-  cout << "hello world!" << endl;
+  zlhywlf::logger::DefaultLoggerFactory f;
+  auto *log = f.createLogger();
+  log->trace("msg");
+  log->debug("msg");
+  log->info("msg");
+  log->warn("msg");
+  log->error("msg");
+  delete log;
   return 0;
 }
