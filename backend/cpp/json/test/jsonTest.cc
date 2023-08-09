27bfb7e168a3c36162a2858cac580537789b2abe
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-08-09 18:21:41 +0800

    chore: 添加 class 类型 json 映射

--- backend/cpp/json/test/jsonTest.cc
+++ backend/cpp/json/test/jsonTest.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "json/util/JsonUtil.H"
+#include "model/Data.H"
 
 using std::cout;
 using std::endl;
@@ -8,8 +9,8 @@ using std::endl;
 int main(int argc, char const *argv[]) {
   const char *json =
       R"({"hello":"world","t":true,"n":null,"i":123,"pi":3.1415926,"a":[0,1,2,3]})";
-  int i;
-  zlhywlf::json::JsonUtil::readValue(i, json);
-  assert(i == 123);
-  return 0;
+  Data data;
+  cout << data.serializedKey() << endl;
+  zlhywlf::json::JsonUtil::readValue(data, json);
+  assert(data.i == 123);
 }
