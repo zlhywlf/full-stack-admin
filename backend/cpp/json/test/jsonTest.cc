e2a9d78d27c6d6afb7ee1964ede4696cbed2e8bf
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-08-09 15:57:42 +0800

    chore: 简化 json 解析

--- backend/cpp/json/test/jsonTest.cc
+++ backend/cpp/json/test/jsonTest.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "rapidjson/document.h"
+#include "json/util/JsonUtil.H"
 
 using std::cout;
 using std::endl;
@@ -8,8 +8,8 @@ using std::endl;
 int main(int argc, char const *argv[]) {
   const char *json =
       R"({"hello":"world","t":true,"n":null,"i":123,"pi":3.1415926,"a":[0,1,2,3]})";
-  rapidjson::Document doc;
-  doc.Parse(json);
-  assert(doc.HasMember("hello"));
+  int i;
+  zlhywlf::json::JsonUtil::readValue(i, json);
+  assert(i == 123);
   return 0;
 }
