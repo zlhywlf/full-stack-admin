eb45d4ba64ddc188bf35a2464c2706b5cb15fde7
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-08-11 11:27:18 +0800

    chore: 添加所有基本类型映射

--- backend/cpp/json/test/jsonTest.cc
+++ backend/cpp/json/test/jsonTest.cc
@@ -10,7 +10,7 @@ using zlhywlf::json::Util::writeValueAsString;
 
 int main(int argc, char const *argv[]) {
   const char *json =
-      R"({"e":0,"hello":"world","b":true,"n":null,"i":123,"d":3.1415926,"a":[0,1,2,3],"sub":{"i":99}})";
+      R"({"i":123,"d":3.1415926,"sub":{"i":99},"e":0,"b":true,"ll":999999999999999,"ls":[1,2],"m":{"i":11},"s":"string","ui":3029196080,"ull":140701862845600,"v":[1,"str",true,[2,"any"]]})";
   Data data;
   cout << "serializedKey: " << data.serializedKey() << endl;
   readValue(data, json);
