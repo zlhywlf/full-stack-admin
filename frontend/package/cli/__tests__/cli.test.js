d8b859e353466a01d5c82f517fd7903a851d6016
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-09-04 19:30:54 +0800

    chore: cli 测试简化

--- frontend/package/cli/__tests__/cli.test.js
+++ frontend/package/cli/__tests__/cli.test.js
@@ -2,9 +2,25 @@ import { test, expect } from "@jest/globals";
 import { execa } from "execa";
 import { resolve } from "path";
 
-const CLI = resolve(process.cwd(), ".", "bin") + "/th-cli.js";
+const run = args =>
+  execa(resolve(process.cwd(), ".", "bin") + "/th-cli.js", args);
 
 test("未知命令", async () => {
-  const { stderr } = await execa(CLI, ["any"]);
+  const { stderr } = await run(["any"]);
   expect(stderr).toContain("命令不存在");
+});
+
+test("help 命令", async () => {
+  let err = null;
+  try {
+    await run(["-h"]);
+  } catch (e) {
+    err = e;
+  }
+  expect(err).toBe(null);
+});
+
+test("版本号", async () => {
+  const { stdout } = await run(["-V"]);
+  expect(stdout).toContain((await import("../package.json")).default.version);
 });
